a68c86cc46ea796bea32d4cc05fd70a1611d145a
Marco Cheung <73602420+LAFLAMIE1024@users.noreply.github.com>
2022-08-15 22:59:14 +0800

    Update net.py

--- net.py
+++ net.py
@@ -19,13 +19,16 @@ class Block(nn.Module):
 class sphere(nn.Module):
     def __init__(self, type=20, is_gray=False):
         super(sphere, self).__init__()
+        
         block = Block
+        
         if type is 20:
             layers = [1, 2, 4, 1]
         elif type is 64:
             layers = [3, 7, 16, 3]
         else:
             raise ValueError('sphere' + str(type) + " IS NOT SUPPORTED! (sphere20 or sphere64)")
+            
         filter_list = [3, 64, 128, 256, 512]
         if is_gray:
             filter_list[0] = 1
@@ -34,7 +37,7 @@ class sphere(nn.Module):
         self.layer2 = self._make_layer(block, filter_list[1], filter_list[2], layers[1], stride=2)
         self.layer3 = self._make_layer(block, filter_list[2], filter_list[3], layers[2], stride=2)
         self.layer4 = self._make_layer(block, filter_list[3], filter_list[4], layers[3], stride=2)
-        self.fc = nn.Linear(512 * 7 * 6, 512)
+        self.fc     = nn.Linear(512 * 7 * 6, 512)
 
         # Weight initialization
         for m in self.modules():
@@ -48,19 +51,22 @@ class sphere(nn.Module):
 
     def _make_layer(self, block, inplanes, planes, blocks, stride):
         layers = []
+        
         layers.append(nn.Conv2d(inplanes, planes, 3, stride, 1))
         layers.append(nn.PReLU(planes))
+        
         for i in range(blocks):
             layers.append(block(planes))
 
         return nn.Sequential(*layers)
 
     def forward(self, x):
+        
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-
+        
         x = x.view(x.size(0), -1)
         x = self.fc(x)
 
@@ -78,6 +84,7 @@ class sphere(nn.Module):
 class BlockIR(nn.Module):
     def __init__(self, inplanes, planes, stride, dim_match):
         super(BlockIR, self).__init__()
+        
         self.bn1 = nn.BatchNorm2d(inplanes)
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
@@ -116,18 +123,23 @@ class LResNet(nn.Module):
     def __init__(self, block, layers, filter_list, is_gray=False):
         self.inplanes = 64
         super(LResNet, self).__init__()
+        
         # input is (mini-batch,3 or 1,112,96)
         # use (conv3x3, stride=1, padding=1) instead of (conv7x7, stride=2, padding=3)
+        
         if is_gray:
             self.conv1 = nn.Conv2d(1, filter_list[0], kernel_size=3, stride=1, padding=1, bias=False)  # gray
         else:
             self.conv1 = nn.Conv2d(3, filter_list[0], kernel_size=3, stride=1, padding=1, bias=False)
+            
         self.bn1 = nn.BatchNorm2d(filter_list[0])
         self.prelu1 = nn.PReLU(filter_list[0])
+        
         self.layer1 = self._make_layer(block, filter_list[0], filter_list[1], layers[0], stride=2)
         self.layer2 = self._make_layer(block, filter_list[1], filter_list[2], layers[1], stride=2)
         self.layer3 = self._make_layer(block, filter_list[2], filter_list[3], layers[2], stride=2)
         self.layer4 = self._make_layer(block, filter_list[3], filter_list[4], layers[3], stride=2)
+        
         self.fc = nn.Sequential(
             nn.BatchNorm1d(filter_list[4] * 7 * 6),
             nn.Dropout(p=0.4),
@@ -155,6 +167,7 @@ class LResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
+        
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.prelu1(x)
